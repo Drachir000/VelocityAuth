d328ca001ca8898eb0714e5a0b63539f2a9122fe
Drachir000 <contact@drachir000.de>
2025-11-01 18:06:37 +0100

    Add bStats support to VelocityAuth
    Implements bStats metrics integration, including configuration parsing from `bstats.json` and metrics registration during plugin initialization.
    
    Signed-off-by: Drachir000 <contact@drachir000.de>

--- src/main/java/de/drachir000/velocity/auth/VelocityAuthPlugin.java
+++ src/main/java/de/drachir000/velocity/auth/VelocityAuthPlugin.java
@@ -6,10 +6,16 @@ import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
+import lombok.AccessLevel;
 import lombok.Getter;
+import org.bstats.velocity.Metrics;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 @Getter
 public class VelocityAuthPlugin {
@@ -18,21 +24,75 @@ public class VelocityAuthPlugin {
 	private final Logger logger;
 	private final Path pluginDirectory;
 	
+	@Getter(AccessLevel.PRIVATE)
+	private final Metrics.Factory metricsFactory;
+	
+	private Metrics metrics;
+	
 	@Inject
-	public VelocityAuthPlugin(ProxyServer server, Logger logger, @DataDirectory Path pluginDirectory) {
+	public VelocityAuthPlugin(ProxyServer server, Logger logger, @DataDirectory Path pluginDirectory, Metrics.Factory metricsFactory) {
 		this.server = server;
 		this.logger = logger;
 		this.pluginDirectory = pluginDirectory;
+		this.metricsFactory = metricsFactory;
 	}
 	
 	@Subscribe
 	public void onProxyInitialization(ProxyInitializeEvent event) {
+		
 		logger.info("Initializing VelocityAuth...");
+		
+		logger.info("Registering bStats metrics...");
+		this.metrics = registerBStats();
+		
 	}
 	
 	@Subscribe
 	public void onProxyShutdown(ProxyShutdownEvent event) {
+		
 		logger.info("Shutting down VelocityAuth...");
+		
+		logger.info("Un-registering bStats metrics...");
+		if (metrics != null) {
+			metrics.shutdown();
+			metrics = null;
+		}
+		
+	}
+	
+	private Metrics registerBStats() {
+		
+		int pluginId = -1;
+		
+		try (
+				InputStream inputStream = this.getClass().getResourceAsStream("bstats.json");
+				Scanner scanner = new Scanner(inputStream).useDelimiter("\\A")
+		) {
+			
+			String jsonString = scanner.hasNext() ? scanner.next() : "{}";
+			JSONObject json = new JSONObject(jsonString);
+			
+			if (!json.has("plugin-id")) {
+				throw new IllegalArgumentException("bstats.json is missing the 'plugin-id' field!");
+			}
+			
+			pluginId = json.optInt("plugin-id", -1);
+			
+			if (pluginId == -1) {
+				throw new IllegalArgumentException("bstats.json has an invalid 'plugin-id' field!");
+			}
+			
+		} catch (IOException | NullPointerException | IllegalArgumentException | SecurityException e) {
+			logger.warn("Failed to register bStats metrics: Failed to load bStats plugin Id!", e);
+			return null;
+		}
+		
+		Metrics metrics = getMetricsFactory().make(this, pluginId);
+		
+		// Custom Charts Here (if any in the future)
+		
+		return metrics;
+		
 	}
 	
 }
